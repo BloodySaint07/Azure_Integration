9598e8cd8040aa365e7da40d8f8e603808a9272a
BloodySaint07 <mandalsoumyajit07@gmail.com>
2023-12-31 22:18:07 +0530

    Logback changes

--- src/main/java/com/example/GetTimeDate/GetTimeDateApplication.java
+++ src/main/java/com/example/GetTimeDate/GetTimeDateApplication.java
@@ -1,5 +1,6 @@
 package com.example.GetTimeDate;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,29 +12,29 @@ import java.util.Date;
 @SpringBootApplication
 public class GetTimeDateApplication {
 
-	public static void getCurrentTimeUsingDate() {
-		Date date = new Date();
-		String strDateFormat = "hh:mm:ss a";
-		DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
-		String formattedDate= dateFormat.format(date);
-		System.out.println("Current time of the day using Date - 12 hour format: " + formattedDate);
-	}
-	public static void getCurrentTimeUsingCalendar() {
-		Calendar cal = Calendar.getInstance();
-		Date date=cal.getTime();
-		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-		String formattedDate=dateFormat.format(date);
-		System.out.println("Current time of the day using Calendar - 24 hour format: "+ formattedDate);
-	}
+    public static void getCurrentTimeUsingDate() {
+        Date date = new Date();
+        String strDateFormat = "hh:mm:ss a";
+        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+        String formattedDate = dateFormat.format(date);
+        System.out.println("Current time of the day using Date - 12 hour format: " + formattedDate);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(GetTimeDateApplication.class, args);
+    public static void getCurrentTimeUsingCalendar() {
+        Calendar cal = Calendar.getInstance();
+        Date date = cal.getTime();
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        String formattedDate = dateFormat.format(date);
+        System.out.println("Current time of the day using Calendar - 24 hour format: " + formattedDate);
+    }
 
-             getCurrentTimeUsingDate();
-		     getCurrentTimeUsingCalendar();
+    public static void main(String[] args) {
+        SpringApplication.run(GetTimeDateApplication.class, args);
+        ApplicationInsights.attach();
+        getCurrentTimeUsingDate();
+        getCurrentTimeUsingCalendar();
 
 
-
-	}
+    }
 
 }
--- src/main/java/com/example/GetTimeDate/TimeController.java
+++ src/main/java/com/example/GetTimeDate/TimeController.java
@@ -1,6 +1,14 @@
 package com.example.GetTimeDate;
 
 
+//import org.apache.logging.log4j.LogManager;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +20,22 @@ import java.time.LocalDateTime;
 @RequestMapping("/gettime/api")
 public class TimeController {
 
+    // SLF4j
+    // Logger logger= LoggerFactory.getLogger(TimeController.class);
+    //Lg4j
+    // Logger logger= LogManager.getLogger(TimeController.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(TimeController.class);
+
     @GetMapping("/time")
-    public ResponseEntity<?> getTime(){
-       return ResponseEntity.ok(LocalDateTime.now());
+    public ResponseEntity<?> getTime() {
+        logger.info("*** /gettime/api/time *** called in TimeController class ");
+
+        return ResponseEntity.ok(LocalDateTime.now());
     }
+
     @GetMapping("/ping")
-    public String ping(){
+    public String ping() {
         return "Time Service Up!!!";
     }
 }
